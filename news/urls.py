3ecdfc04db7ee49d9a1f20cee72985c58efb1d94
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-06 21:56:20 +0300

    add commits

---
+++ news/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from django.conf.urls import url
+from . import views
+
+urlpatterns=[
+    # path('',views.welcome,name = 'welcome'),
+    path('',views.news_of_day,name='newsToday'),
+    path('archives/<past_date>/',views.past_days_news,name = 'pastNews'), 
+    path('search/', views.search_results, name='search_results'),
+    path('article/<article_id>/',views.article, name ='article')
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
